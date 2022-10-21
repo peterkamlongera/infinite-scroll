81ea230177e461efa8362294079c201f38bf6da9
peterkamlongera <peterkamlongera@gmail.com>
2022-10-21 03:04:42 +0200

    loader optimization

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 //  Unsplash API
-const count = 30;
+const count = 5;
 const apiKey = 'BHNmRIYH3uXEBBlkzQfXeOQsrrfKr1-moxXGehHghnY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
@@ -17,6 +17,7 @@ function imageLoaded() {
     if (imagesLoaded === totalImages) {
         ready = true;
         laoder.hidden = true;
+        count  = 30;
     }
 }
 
